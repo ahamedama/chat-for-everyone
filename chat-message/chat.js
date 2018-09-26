76c162f31e2c4f1611d07397efe36abf5986b905
ahamedama <ahamedama@gmail.com>
2018-09-27 01:53:37 +0530

    Initial-commit
    This is the very very basic UI design setup for chat.

---
+++ chat-message/chat.js
@@ -0,0 +1,8 @@
+/*  
+Author & owner : Ahamed,
+This is the property os ahamedama Inc.,
+Warning : Don't edit this block of comments. You can add yours below the mark.
+*/
+// ----------------------------------------------------------------------------
+
+// Not yet coded. Feel free to contribute
